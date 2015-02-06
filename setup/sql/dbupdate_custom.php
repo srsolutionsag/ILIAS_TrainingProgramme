cfb39bcca2ca8c460613cfba33533373827d6960
Richard Klees <richard.klees@concepts-and-training.de>
2015-02-06 12:19:21 +0100

    added sequence and primary keys to table structure.

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -10,3 +10,18 @@ ilTrainingProgrammeAssignment::installDB();
 ilTrainingProgrammeProgress::installDB();
 
 ?>
+
+<#2>
+<?php
+
+require_once("./Modules/TrainingProgramme/classes/model/class.ilTrainingProgrammeProgress.php");
+require_once("./Modules/TrainingProgramme/classes/model/class.ilTrainingProgrammeAssignment.php");
+
+// Active Record does not support tuples as primary keys, so we have to
+// set those on our own.
+$ilDB->dropPrimaryKey(ilTrainingProgrammeProgress::returnDbTableName());
+$ilDB->addPrimaryKey( ilTrainingProgrammeProgress::returnDbTableName()
+					, array("assignment_id", "prg_id", "usr_id")
+					);
+
+?>
